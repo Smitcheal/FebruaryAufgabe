7f63cb5b2c9ab211b4b5430ff9ff5af52aed1ac0
unknown <smitcheal431@gmail.com>
2024-05-10 12:24:08 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,137 @@
+
+// datatypes
+// 2 main datatypes which are the primitive and object datatypes/
+// primitives types are: 
+// 1 numbers
+// 2 strings
+// 3 boolean
+// 4 undefined
+// 5 null
+// 6 big int
+// 7 symbol
+
+
+// numbers
+let num = 23;
+console.log(num);
+
+
+// strings
+let student = "obi";
+console.log(student);
+
+let greetings = "whats'up"
+console.log(greetings);
+
+// boolean
+let g = true;
+
+// undefined
+let b;
+console.log(b);
+// null
+let hello = document.querySelector("ul");
+
+// big init
+
+
+// symbol
+
+// variables
+
+//LET
+let age = 20;
+console.log(age);
+
+age = 21;
+console.log(age);
+
+let color = "green";
+console.log(color)
+
+
+// CONST
+const birthYear = 1900;
+console.log(birthYear);
+
+// VAR
+var job = "programmer";
+console.log(job);
+job = "teacher";
+console.log(job);
+// operators
+// block scope
+{
+let age = 20;
+console.log(typeof age);
+}
+
+{
+    let age ="age";
+    console.log(age);
+}
+
+const numb = "23";
+console.log(typeof numb)
+
+// carmel case
+const birthYearOfMe = 1900;
+console.log(birthYearOfMe);
+
+const lastName = 'f'
+
+// reserved key word
+// const new = 33;
+
+let MYname = "somtoo";
+
+// ARITHEMETIC 
+console.log(2 + 4);
+console.log(4 - 39);
+const year = 2024;
+const ageNigeria = year - 1960;
+console.log("Nigeria age", ageNigeria);
+console.log(year / 10);
+console.log(year * 2)
+
+// MODULUS
+console.log(7 % 2);
+console.log (10 % 4);
+
+
+// Increment
+let x = 10;
+console.log(x++);
+console.log(x);
+
+// decrement
+console.log(x--)
+console.log(x);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// {
+//     var job = "programmer";
+//     console.log(job);
+// }
+
+
+
+// 2 0bject datatypes
+// 1 
+// 2
+// 3
